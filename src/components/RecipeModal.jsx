6ea46fd50e261a9301d760208d38f80f24dcd505
Mark Taratynov <mark.taratynov@gmail.com>
2024-11-12 15:51:36 +0100

    test video tag

--- src/components/RecipeModal.jsx
+++ src/components/RecipeModal.jsx
@@ -1,32 +1,56 @@
+import { useRef, useEffect } from "react";
+
 const RecipeModal = ({ recipe, onClose }) => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+  const videoRef = useRef(null);
+
   return (
     <div className="modal-backdrop" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <button className="close-button" onClick={onClose}>
           ✖️
         </button>
-        <h2>{recipe.name}</h2>
-        <p>
-          <strong>Ingredients:</strong> {recipe.ingredients.join(", ")}
-        </p>
-        <p>
-          <strong>Preparation:</strong> {recipe.preparation}
-        </p>
-        <p>
-          <strong>Preparation Time:</strong> {recipe.preparation_time} min
-        </p>
-        <p>
-          <strong>Cooking Time:</strong> {recipe.cooking_time} min
-        </p>
-        <p>
-          <strong>Total Time:</strong> {recipe.total_time} min
-        </p>
-        <p>
-          <strong>Servings:</strong> {recipe.servings}
-        </p>
-        <p>
-          <strong>Difficulty:</strong> {recipe.difficulty}
-        </p>
+        <h2 className="modal-title">{recipe.name}</h2>
+        <div className="modal-body">
+          <p>
+            <strong>Ingredients:</strong> {recipe.ingredients.join(", ")}
+          </p>
+          <p>
+            <strong>Preparation:</strong> {recipe.preparation}
+          </p>
+          <p>
+            <strong>Preparation Time:</strong> {recipe.preparation_time} min
+          </p>
+          <p>
+            <strong>Cooking Time:</strong> {recipe.cooking_time} min
+          </p>
+          <p>
+            <strong>Total Time:</strong> {recipe.total_time} min
+          </p>
+          <p>
+            <strong>Servings:</strong> {recipe.servings}
+          </p>
+          <p>
+            <strong>Difficulty:</strong> {recipe.difficulty}
+          </p>
+          <div className="video-container">
+            <video
+              ref={videoRef}
+              width={300}
+              controls
+              controlsList="nodownload"
+              onContextMenu={(e) => e.preventDefault()}
+              src="src/assets/img/test-video.mov"
+              type="video/mov"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
