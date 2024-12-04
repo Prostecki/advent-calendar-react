9a465aa0a2ffc66d34a1f5afd09e35855ee391cd
Mark Taratynov <mark.taratynov@gmail.com>
2024-12-04 23:03:05 +0100

    just a change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <section className="bg-beige w-full py-5">
-      <h2 className="text-browny text-center font-normal">
+    <section className="bg-beige w-full py-5 drop-shadow-lg">
+      <h2 className="text-browny text-center text-lg drop-shadow-lg">
         All rights reserved. Anastasia Taratynova - 2024
       </h2>
     </section>
